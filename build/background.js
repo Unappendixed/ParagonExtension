0667856be88d807db71d2a415fe46e9ca6f41472
Brad Carlson <brad@bscarlson.com>
2022-04-20 00:44:31 -0700

    Removed build folder from tracking and updated .gitignore accordingly

--- build/background.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-chrome.browserAction.setPopup({ popup: '' }); //disable browserAction's popup
-chrome.browserAction.onClicked.addListener(() => {
-    chrome.tabs.create({ url: 'public/options.html' });
-});
